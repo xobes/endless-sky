f345e4def03c1fe159076a6d21522413f17916a7
xobes <ajzobro@gmail.com>
2025-06-18 21:05:35 -0500

    whitespace

--- source/ShipyardPanel.cpp
+++ source/ShipyardPanel.cpp
@@ -392,7 +392,7 @@ void ShipyardPanel::SellShip(bool storeOutfits)
 void ShipyardPanel::DrawButtons()
 {
 	// There will be two rows of buttons:
-	//  [    Buy    ] [    Sell    ] [ Sell Hull ] 
+	//  [    Buy    ] [    Sell    ] [ Sell Hull ]
 	//                               [   Leave   ]
 	const double rowOffsetY = BUTTON_HEIGHT + BUTTON_ROW_PAD;
 	const double rowBaseY = Screen::BottomRight().Y() - 1.5 * rowOffsetY - BUTTON_ROW_START_PAD;
